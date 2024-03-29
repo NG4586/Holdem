90c695561e5735080161cd39827fce2ddad4e188
NG4586 <sevargetan@gmail.com>
2024-03-29 16:22:50 -0600

    Initial Commit

---
+++ Holdem/HoldemApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoldemApp.swift
+//  Holdem
+//
+//  Created by Nathaniel Graves on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct HoldemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
